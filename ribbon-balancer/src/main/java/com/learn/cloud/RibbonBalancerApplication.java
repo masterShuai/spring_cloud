e8aab239c935ef4e9b7b61c6561f5a772c8eaf7d
wangshuai <wangshuai@quyiyuan.com>
2017-07-17 17:54:12 +0800

    0717-2 Hystrix Dashboard

--- ribbon-balancer/src/main/java/com/learn/cloud/RibbonBalancerApplication.java
+++ ribbon-balancer/src/main/java/com/learn/cloud/RibbonBalancerApplication.java
@@ -5,12 +5,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableDiscoveryClient //向服务中心注册
 @EnableHystrix //允许断路器
+@EnableHystrixDashboard //允许断路器仪表盘
 public class RibbonBalancerApplication {
 
 	public static void main(String[] args) {
